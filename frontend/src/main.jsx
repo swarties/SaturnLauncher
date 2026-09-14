14d3a2752f0e85904eb0494683c66cfee4f6d136
swarties <76826079+swarties@users.noreply.github.com>
2026-09-14 12:28:28 -0400

    Added front-end dependencies and updated GitIgnore

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import {createRoot} from 'react-dom/client'
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import {routeTree} from './routeTree.gen.ts'
 import './style.css'
-import App from './App'
 
 const router = createRouter({
     routeTree,
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,7 @@
+import path from 'path'
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
@@ -9,5 +11,11 @@ export default defineConfig({
         target: 'react',
           autoCodeSplitting: true,
       }),
-      react(),]
+      react(),
+      tailwindcss(),
+  ], resolve: {
+      alias: {
+          '@': path.resolve(__dirname, './src'),
+      },
+    },
 })
