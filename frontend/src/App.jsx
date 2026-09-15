e9ced12361e42ce14187a700b3c8f8f95a49753f
swarties <76826079+swarties@users.noreply.github.com>
2026-09-15 19:43:48 -0400

    runtimefix on frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import logo from './assets/images/logo-universal.png';
 import './App.css';
-import { Greet } from '../wailsjs/go/main/App';
 
 function App() {
   return <div id="App"></div>;
