09642c38ecedb68b2928b375f2247db3c6e75475
SlowOnARocketTM <181799226+SlowOnARocketTM@users.noreply.github.com>
2026-09-15 23:25:52 -0400

    init new account.go file for future updates add events and startlogin for frontend implementation

---
+++ account.go
@@ -0,0 +1,6 @@
+package main
+
+// add update refresh key + mctoken for when lauching
+// add them in the json
+// verifies user authenticity func for that takes MinecraftPayload and we put it in the fun in app.go
+// get user uuid + username + maybe head of skin png ?
--- app.go
+++ app.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+
+	wailsRuntime "github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 // App struct
@@ -20,6 +22,48 @@ func NewApp() *App {
 // so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
+}
+
+func (a *App) StartLogin() {
+	go func() {
+		// 1. Get OAuth Device Code
+		payload, err := a.Auth.GetOAuthCode()
+		if err != nil {
+			wailsRuntime.EventsEmit(a.ctx, "login:error", err.Error())
+		}
+		// Send code & URL to frontend to render
+		wailsRuntime.EventsEmit(a.ctx, "login:send_received", payload)
+		// 2. Poll for Access Token (blocking call)
+		at, err := a.Auth.PollOAuthCode(*payload)
+		if err != nil {
+			wailsRuntime.EventsEmit(a.ctx, "login:error", err.Error())
+		}
+		// 3. Exchange tokens through Xbox Live and Minecraft Services
+		xbt, err := a.Auth.GetXBL(*at)
+		if err != nil {
+			wailsRuntime.EventsEmit(a.ctx, "login:error", err.Error())
+		}
+
+		xsts, err := a.Auth.GetXSTS(*xbt)
+		if err != nil {
+			wailsRuntime.EventsEmit(a.ctx, "login:error", err.Error())
+		}
+
+		mctoken, err := a.Auth.GetMinecraftAuth(*xsts, *xbt)
+		if err != nil {
+			wailsRuntime.EventsEmit(a.ctx, "login:error", err.Error())
+		}
+		// 4. Save Refresh Session
+		if len(xbt.DisplayClaims.Xui) > 0 {
+			session := AuthSession{
+				RefreshToken: at.RefreshToken,
+				Uhs:          xbt.DisplayClaims.Xui[0].Uhs,
+			}
+			_, _ = a.Auth.SaveKeysToJson(session)
+		}
+		// 5. Notify frontend on completion
+		wailsRuntime.EventsEmit(a.ctx, "login:success", nil) // replace nil with a map containing user uuid and username also add a ownership check that if fails returns login:error
+	}()
 }
 
 /*
