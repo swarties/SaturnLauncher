61593e74e3af4d43afb05fce44a52a6e2ef32344
swarties <76826079+swarties@users.noreply.github.com>
2026-09-23 13:32:40 -0400

    - Scaffold Home.jsx

--- frontend/src/routes/home.jsx
+++ frontend/src/routes/home.jsx
@@ -1,9 +1,19 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { Button } from '@/components/ui/button.jsx';
 
 export const Route = createFileRoute('/home')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/home"!</div>;
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <h1>HI</h1>
+      <h2>Helllo</h2>
+      <Button>Boom</Button>
+      <Button>Launch Minecraft</Button>
+    </div>
+  );
 }
+
+// WIP divs in divs for flex layout or the other thing
