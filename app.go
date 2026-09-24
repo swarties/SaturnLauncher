0e1ae9c4b79a71f38f41102441a9e269ca6dd56c
SlowOnARocketTM <181799226+SlowOnARocketTM@users.noreply.github.com>
2026-09-23 22:12:40 -0400

    return errors

--- app.go
+++ app.go
@@ -179,20 +179,21 @@ func (a *App) StartApp() {
 // logout deletes the 2 .jsons and clears all the vals
 // AuthSession struct for unmarshalling JSON
 
-func (a *App) GetGameFiles() {
+func (a *App) GetGameFiles() error {
 	manifest, err := a.Download.GetVersionManifest()
 	if err != nil {
-		return
+		return err
 	}
 	err = a.Download.GetVersionInfo(*manifest, "26.3")
 	if err != nil {
-		return
+		return err
 	}
 
 	err = a.Download.GetClientJar("26.3")
 	if err != nil {
-		return
+		return err
 	}
 
 	fmt.Println("Debug func cuz no ui and download flow still needs libs + assets to run check files for client.jar and json file ")
+	return nil
 }
